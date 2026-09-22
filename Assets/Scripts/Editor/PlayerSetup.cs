40f6113014a2e9c0ba27010ea52db86898b3da8d
gekkin-programmer <nkouambrayan@gmail.com>
2026-09-22 16:26:07 +0100

    Ajout Tilemap avec tiles Kenney pour la carte

--- Assets/Scripts/Editor/PlayerSetup.cs
+++ Assets/Scripts/Editor/PlayerSetup.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine.Tilemaps;
 
 public class PlayerSetup : EditorWindow
 {
@@ -16,7 +17,7 @@ public class PlayerSetup : EditorWindow
             Object.DestroyImmediate(dirLight.gameObject);
         }
 
-        // Setup Camera - TOP DOWN VIEW
+        // Setup Camera - TOP DOWN
         Camera cam = Camera.main;
         cam.orthographic = true;
         cam.orthographicSize = 12f;
@@ -24,20 +25,18 @@ public class PlayerSetup : EditorWindow
         cam.transform.rotation = Quaternion.Euler(90f, 0, 0);
         cam.backgroundColor = Color.black;
         cam.clearFlags = CameraClearFlags.SolidColor;
-        cam.nearClipPlane = 0.1f;
-        cam.farClipPlane = 100f;
 
         CameraFollow camFollow = cam.gameObject.AddComponent<CameraFollow>();
 
         SetupPlayer(camFollow);
         SetupGameManager();
-        SetupDarkMap();
+        SetupTilemapMap();
 
         string scenePath = "Assets/Scenes/MainScene.unity";
         System.IO.Directory.CreateDirectory("Assets/Scenes");
         EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene(), scenePath);
 
-        Debug.Log("Scène créée - vue de haut!");
+        Debug.Log("Carte Tilemap créée!");
     }
 
     static void SetupPlayer(CameraFollow camFollow)
@@ -60,7 +59,6 @@ public class PlayerSetup : EditorWindow
         player.AddComponent<Shooting>();
         player.AddComponent<Health>();
 
-        // Light Halo
         GameObject lightObj = new GameObject("HaloLight");
         lightObj.transform.SetParent(player.transform);
         lightObj.transform.localPosition = new Vector3(0, 5, 0);
@@ -75,9 +73,7 @@ public class PlayerSetup : EditorWindow
         player.transform.position = new Vector3(0, 0, 0);
 
         if (camFollow != null)
-        {
             camFollow.target = player.transform;
-        }
     }
 
     static void SetupGameManager()
@@ -86,9 +82,76 @@ public class PlayerSetup : EditorWindow
         gm.AddComponent<GameManager>();
     }
 
-    static void SetupDarkMap()
+    static void SetupTilemapMap()
     {
-        // Ground
+        // Create Tilemap Grid
+        GameObject grid = new GameObject("Grid");
+        Grid gridComp = grid.AddComponent<Grid>();
+        gridComp.cellSize = new Vector3(1, 1, 1);
+
+        // Ground Tilemap
+        GameObject groundTilemap = new GameObject("GroundTilemap");
+        groundTilemap.transform.SetParent(grid.transform);
+        Tilemap groundMap = groundTilemap.AddComponent<Tilemap>();
+        TilemapRenderer groundRenderer = groundTilemap.AddComponent<TilemapRenderer>();
+        groundRenderer.sortingOrder = 0;
+        groundTilemap.AddComponent<TilemapCollider2D>();
+
+        // Walls Tilemap
+        GameObject wallsTilemap = new GameObject("WallsTilemap");
+        wallsTilemap.transform.SetParent(grid.transform);
+        Tilemap wallsMap = wallsTilemap.AddComponent<Tilemap>();
+        TilemapRenderer wallsRenderer = wallsTilemap.AddComponent<TilemapRenderer>();
+        wallsRenderer.sortingOrder = 1;
+        wallsTilemap.AddComponent<TilemapCollider2D>();
+
+        // Load tiles
+        TileBase floorTile = AssetDatabase.LoadAssetAtPath<TileBase>("Assets/Kenney/PNG/Tiles/tile_01.png");
+        TileBase wallTile = AssetDatabase.LoadAssetAtPath<TileBase>("Assets/Kenney/PNG/Tiles/tile_03.png");
+        TileBase wallCornerTile = AssetDatabase.LoadAssetAtPath<TileBase>("Assets/Kenney/PNG/Tiles/tile_04.png");
+
+        if (floorTile == null || wallTile == null)
+        {
+            Debug.Log("Tiles non trouvés - utilisation de sprites simples");
+            CreateSimpleMap(grid);
+            return;
+        }
+
+        // Fill ground
+        for (int x = -20; x <= 20; x++)
+        {
+            for (int y = -20; y <= 20; y++)
+            {
+                groundMap.SetTile(new Vector3Int(x, y, 0), floorTile);
+            }
+        }
+
+        // Create walls
+        for (int x = -20; x <= 20; x++)
+        {
+            wallsMap.SetTile(new Vector3Int(x, 20, 0), wallTile);
+            wallsMap.SetTile(new Vector3Int(x, -20, 0), wallTile);
+        }
+        for (int y = -20; y <= 20; y++)
+        {
+            wallsMap.SetTile(new Vector3Int(20, y, 0), wallTile);
+            wallsMap.SetTile(new Vector3Int(-20, y, 0), wallTile);
+        }
+
+        // Add some random obstacles
+        for (int i = 0; i < 15; i++)
+        {
+            int x = Random.Range(-15, 15);
+            int y = Random.Range(-15, 15);
+            wallsMap.SetTile(new Vector3Int(x, y, 0), wallCornerTile);
+        }
+
+        Debug.Log("Tilemap Map created with Kenney tiles!");
+    }
+
+    static void CreateSimpleMap(GameObject grid)
+    {
+        // Fallback: use sprites on ground
         GameObject ground = GameObject.CreatePrimitive(PrimitiveType.Plane);
         ground.name = "Ground";
         ground.transform.localScale = new Vector3(10, 1, 10);
@@ -97,13 +160,11 @@ public class PlayerSetup : EditorWindow
         Object.DestroyImmediate(ground.GetComponent<BoxCollider>());
         ground.GetComponent<Renderer>().sharedMaterial.color = new Color(0.02f, 0.02f, 0.02f);
 
-        // Walls
         CreateWall("WallNorth", new Vector3(0, 0.5f, 50), new Vector3(100, 1, 1));
         CreateWall("WallSouth", new Vector3(0, 0.5f, -50), new Vector3(100, 1, 1));
         CreateWall("WallEast", new Vector3(50, 0.5f, 0), new Vector3(1, 1, 100));
         CreateWall("WallWest", new Vector3(-50, 0.5f, 0), new Vector3(1, 1, 100));
 
-        // Cover
         for (int i = 0; i < 10; i++)
         {
             float x = Random.Range(-35f, 35f);
