68834e215ac74ce7a63eb051957a397037ce4846
gekkin-programmer <nkouambrayan@gmail.com>
2026-09-22 18:54:43 +0100

    Fix fog overlay - trou transparent autour du joueur

--- Assets/Scripts/Player/DarknessOverlay.cs
+++ Assets/Scripts/Player/DarknessOverlay.cs
@@ -5,73 +5,42 @@ public class DarknessOverlay : MonoBehaviour
     public float haloRadius = 12f;
     public LayerMask occluderMask;
 
-    private MeshFilter meshFilter;
-    private MeshRenderer meshRenderer;
-    private Mesh fogMesh;
     private Material fogMat;
-
-    private int segments = 64;
-    private float fogSize = 100f;
+    private GameObject fogQuad;
 
     void Start()
     {
-        GameObject fogObj = new GameObject("Fog");
-        fogObj.transform.SetParent(transform);
-        fogObj.transform.localPosition = new Vector3(0, 0, 0.5f);
+        fogQuad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        fogQuad.name = "FogOverlay";
+        fogQuad.transform.SetParent(transform);
+        fogQuad.transform.localPosition = new Vector3(0, 0, 0.5f);
+        fogQuad.transform.localScale = new Vector3(200, 200, 1);
 
-        meshFilter = fogObj.AddComponent<MeshFilter>();
-        meshRenderer = fogObj.AddComponent<MeshRenderer>();
+        // Remove collider
+        Object.Destroy(fogQuad.GetComponent<BoxCollider>());
+
+        // Setup renderer
+        MeshRenderer mr = fogQuad.GetComponent<MeshRenderer>();
 
         Shader shader = Shader.Find("Custom/FogOverlay");
         if (shader == null)
             shader = Shader.Find("Sprites/Default");
 
         fogMat = new Material(shader);
-        fogMat.color = Color.black;
+        fogMat.SetFloat("_Radius", haloRadius);
+        fogMat.SetVector("_PlayerPos", Vector4.zero);
 
-        meshRenderer.material = fogMat;
-        meshRenderer.sortingLayerName = "Fog";
-        meshRenderer.sortingOrder = 100;
-
-        fogMesh = new Mesh();
-        fogMesh.name = "FogMesh";
-        meshFilter.mesh = fogMesh;
-
-        GenerateFogMesh();
+        mr.material = fogMat;
+        mr.sortingLayerName = "Fog";
+        mr.sortingOrder = 100;
     }
 
     void LateUpdate()
     {
-        GenerateFogMesh();
-    }
-
-    void GenerateFogMesh()
-    {
-        Vector3 center = transform.position;
-        float r = fogSize;
-
-        Vector3[] vertices = new Vector3[segments + 1];
-        int[] triangles = new int[segments * 3];
-
-        vertices[0] = new Vector3(0, 0, 0);
-
-        for (int i = 0; i < segments; i++)
+        if (fogMat != null)
         {
-            float angle = (float)i / segments * Mathf.PI * 2f;
-            vertices[i + 1] = new Vector3(Mathf.Cos(angle) * r, Mathf.Sin(angle) * r, 0);
+            fogMat.SetVector("_PlayerPos", transform.position);
         }
-
-        for (int i = 0; i < segments; i++)
-        {
-            triangles[i * 3] = 0;
-            triangles[i * 3 + 1] = i + 1;
-            triangles[i * 3 + 2] = (i + 1) % segments + 1;
-        }
-
-        fogMesh.Clear();
-        fogMesh.vertices = vertices;
-        fogMesh.triangles = triangles;
-        fogMesh.RecalculateNormals();
     }
 
     void OnDrawGizmosSelected()
--- Assets/Shaders/FogOverlay.shader
+++ Assets/Shaders/FogOverlay.shader
@@ -1,5 +1,12 @@
 Shader "Custom/FogOverlay"
 {
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Radius ("Radius", Float) = 12
+        _PlayerPos ("Player Position", Vector) = (0, 0, 0, 0)
+        _Color ("Fog Color", Color) = (0, 0, 0, 0.95)
+    }
     SubShader
     {
         Tags { "Queue"="Overlay" "RenderType"="Transparent" }
@@ -20,23 +27,43 @@ Shader "Custom/FogOverlay"
             struct appdata
             {
                 float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
             };
 
             struct v2f
             {
+                float2 uv : TEXCOORD0;
                 float4 vertex : SV_POSITION;
+                float3 worldPos : TEXCOORD1;
             };
+
+            sampler2D _MainTex;
+            float _Radius;
+            float4 _PlayerPos;
+            fixed4 _Color;
 
             v2f vert(appdata v)
             {
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;
                 return o;
             }
 
             fixed4 frag(v2f i) : SV_Target
             {
-                return fixed4(0, 0, 0, 0.95);
+                float2 playerPos2D = _PlayerPos.xy;
+                float2 fragPos2D = i.worldPos.xy;
+
+                float dist = distance(fragPos2D, playerPos2D);
+
+                float holeRadius = _Radius;
+                float edgeWidth = 1.5;
+
+                float alpha = 1.0 - smoothstep(holeRadius - edgeWidth, holeRadius, dist);
+
+                return fixed4(_Color.rgb, _Color.a * alpha);
             }
             ENDCG
         }
