4f6fad97d7c161d9220ff30c5c56898dcd9dea09
gekkin-programmer <nkouambrayan@gmail.com>
2026-09-22 19:00:09 +0100

    Fix shader fog - alpha corrigé, trou transparent autour du joueur

--- Assets/Scripts/Player/DarknessOverlay.cs
+++ Assets/Scripts/Player/DarknessOverlay.cs
@@ -10,29 +10,54 @@ public class DarknessOverlay : MonoBehaviour
 
     void Start()
     {
-        fogQuad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        fogQuad.name = "FogOverlay";
+        // Create a large quad for the fog
+        fogQuad = new GameObject("FogOverlay");
         fogQuad.transform.SetParent(transform);
         fogQuad.transform.localPosition = new Vector3(0, 0, 0.5f);
-        fogQuad.transform.localScale = new Vector3(200, 200, 1);
 
-        // Remove collider
-        Object.Destroy(fogQuad.GetComponent<BoxCollider>());
+        MeshFilter mf = fogQuad.AddComponent<MeshFilter>();
+        MeshRenderer mr = fogQuad.AddComponent<MeshRenderer>();
 
-        // Setup renderer
-        MeshRenderer mr = fogQuad.GetComponent<MeshRenderer>();
+        // Create quad mesh
+        Mesh mesh = new Mesh();
+        float size = 100f;
+        mesh.vertices = new Vector3[]
+        {
+            new Vector3(-size, -size, 0),
+            new Vector3(size, -size, 0),
+            new Vector3(size, size, 0),
+            new Vector3(-size, size, 0)
+        };
+        mesh.uv = new Vector2[]
+        {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(1, 1),
+            new Vector2(0, 1)
+        };
+        mesh.triangles = new int[] { 0, 2, 1, 0, 3, 2 };
+        mesh.RecalculateNormals();
+        mf.mesh = mesh;
 
+        // Load shader
         Shader shader = Shader.Find("Custom/FogOverlay");
         if (shader == null)
+        {
+            Debug.LogError("Shader Custom/FogOverlay not found!");
             shader = Shader.Find("Sprites/Default");
+        }
 
         fogMat = new Material(shader);
         fogMat.SetFloat("_Radius", haloRadius);
         fogMat.SetVector("_PlayerPos", Vector4.zero);
+        fogMat.SetColor("_Color", new Color(0, 0, 0, 0.95f));
+        fogMat.SetFloat("_Softness", 1.5f);
 
         mr.material = fogMat;
         mr.sortingLayerName = "Fog";
         mr.sortingOrder = 100;
+
+        Debug.Log("DarknessOverlay created with radius " + haloRadius);
     }
 
     void LateUpdate()
--- Assets/Shaders/FogOverlay.shader
+++ Assets/Shaders/FogOverlay.shader
@@ -2,10 +2,10 @@ Shader "Custom/FogOverlay"
 {
     Properties
     {
-        _MainTex ("Texture", 2D) = "white" {}
         _Radius ("Radius", Float) = 12
         _PlayerPos ("Player Position", Vector) = (0, 0, 0, 0)
         _Color ("Fog Color", Color) = (0, 0, 0, 0.95)
+        _Softness ("Edge Softness", Float) = 1.5
     }
     SubShader
     {
@@ -27,41 +27,36 @@ Shader "Custom/FogOverlay"
             struct appdata
             {
                 float4 vertex : POSITION;
-                float2 uv : TEXCOORD0;
             };
 
             struct v2f
             {
-                float2 uv : TEXCOORD0;
                 float4 vertex : SV_POSITION;
-                float3 worldPos : TEXCOORD1;
+                float3 worldPos : TEXCOORD0;
             };
 
-            sampler2D _MainTex;
             float _Radius;
             float4 _PlayerPos;
             fixed4 _Color;
+            float _Softness;
 
             v2f vert(appdata v)
             {
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = v.uv;
                 o.worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;
                 return o;
             }
 
             fixed4 frag(v2f i) : SV_Target
             {
-                float2 playerPos2D = _PlayerPos.xy;
                 float2 fragPos2D = i.worldPos.xy;
+                float2 playerPos2D = _PlayerPos.xy;
 
                 float dist = distance(fragPos2D, playerPos2D);
 
-                float holeRadius = _Radius;
-                float edgeWidth = 1.5;
-
-                float alpha = 1.0 - smoothstep(holeRadius - edgeWidth, holeRadius, dist);
+                // Inside hole = transparent, outside = opaque
+                float alpha = smoothstep(_Radius - _Softness, _Radius, dist);
 
                 return fixed4(_Color.rgb, _Color.a * alpha);
             }
